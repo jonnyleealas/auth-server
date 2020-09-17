f5e6392ae31e695337748fcfb50dc6656fd4ba70
jonathon lee <jonnyleealas@gmail.com>
2020-09-17 13:08:18 -0700

    lab13

---
+++ auth-server/users-model.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jsonToken = require('jsonwebtoken');
+
+const users = mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true }
+})
+
+users.pre('save', async function(){
+    this.password = await bcrypt.hash(this.password, 5)
+    console.log(' the password is', this.password)
+})
+
+users.methods.generateToken = async function(){
+    // turns user into an object
+    let token = jsonToken.sign({username: this.username}, process.env.SECRET)
+    return token;
+}
+
+module.exports= mongoose.model('users', users)
